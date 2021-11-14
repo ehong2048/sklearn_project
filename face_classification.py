76927ca0b14a87d03e457af72d1d2d7a763d07e1
ehong2048 <ehong2048@gmail.com>
2021-11-13 21:00:56 -0800

    Created file and added imports

---
+++ face_classification.py
@@ -0,0 +1,7 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import fetch_lfw_people
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import plot_confusion_matrix, mean_squared_error, r2_score
+from matplotlib import pyplot as plt
+import numpy as np
+
