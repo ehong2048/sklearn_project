86391415c98a1c7b0a56608d6a7adebd4288f73a
ehong2048 <ehong2048@gmail.com>
2021-11-14 00:59:01 -0800

    Added README and tried to add PCA for model

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # sklearn_project
+Author: Emma Hong
+11/2021 Open Scikit Learn Project to demonstrate understanding
+
+## Description
+I use skikit-learn's Labeled Faces in the Wild dataset to train a classification model for classifying images of Bush, Powell, Rumsfeld, Schroeder, and Blair, acheiving an accuracy of 86%. Then, I use it to classify downloaded images of the 5 people from the internet to see if it works.
+
+## Usage
+    python face_classification.py --model $model_name$ --image $path_image_to_classify$
+
+model_name are any of the following strings: LogisticRegression, LogisticRegressionCV, PassiveAggressiveClassifier, Perceptron, or SGDClassifier
--- face_classification.py
+++ face_classification.py
@@ -1,12 +1,14 @@
 from sklearn.datasets import fetch_lfw_people
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV, Perceptron, PassiveAggressiveClassifier, SGDClassifier
+from sklearn.decomposition import PCA
 from sklearn.metrics import accuracy_score, plot_confusion_matrix
 from matplotlib import pyplot as plt
 import argparse
 import numpy as np
 import cv2
 
+print("---Celebrity Facial Classification---")
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-m', "--model", required = True, help = "string with which classification model to use: LogisticRegression, LogisticRegressionCV, PassiveAggressiveClassifier, Perceptron, or SGDClassifier")
@@ -14,35 +16,43 @@ parser.add_argument('-i', "--image", help = "str path to image of Powell, Rumsfe
 args = vars(parser.parse_args())
 model = args["model"]
 
-print("Celebrity Facial Classification")
 face_data = fetch_lfw_people(min_faces_per_person=100, resize = 0.4) #fetches face data, only keeps people with at least 70 pictures
 num_images = face_data.images.shape[0]
-print(f'Num Images: {num_images}\nImage Shape: {face_data.images.shape[1]} by {face_data.images.shape[2]}')
+h = face_data.images.shape[1]
+w = face_data.images.shape[2]
+print(f'Num Images: {num_images}\nImage Shape: {h} by {w}')
 print(f'Featured Celebrities: {face_data.target_names}')
 
 trainX, testX, trainY, testY = train_test_split(
     face_data.data, face_data.target, test_size = 0.3, shuffle = False)
 
+"""
+n_components = 150
+pca = PCA(n_components=n_components, whiten=True).fit(trainX)
+eigenfaces = pca.components_.reshape((n_components, h, w))
+
+trainX = pca.transform(trainX)
+testX = pca.transform(testX)
+"""
 
 if model == "LogisticRegression":
-    classifier = LogisticRegression('l2', max_iter=100, solver = 'liblinear', shuffle = False)
+    classifier = LogisticRegression('l2', max_iter=100, solver = 'liblinear')
 
 elif model == "LogisticRegressionCV":
-    classifier = LogisticRegressionCV(penalty = 'l2', max_iter=100, solver = 'liblinear', shuffle = False)
+    classifier = LogisticRegressionCV(penalty = 'l2', max_iter=100, solver = 'liblinear')
 
 elif model == "Perceptron":
-    classifier = Perceptron(penalty = 'l1', alpha = 0.14, max_iter=100, shuffle = False)
+    classifier = Perceptron(penalty = 'l1', alpha = 0.14, max_iter=100)
 
 elif model == "PassiveAggressiveClassifier":
-    classifier = PassiveAggressiveClassifier(max_iter=100, shuffle = False)
+    classifier = PassiveAggressiveClassifier(max_iter=100)
 
 elif model == "SGDClassifier":
-    classifier = SGDClassifier(alpha = 0.12, eta0 = 0.04, learning_rate = "adaptive", tol = 0.001,  max_iter=1000, shuffle = False)
+    classifier = SGDClassifier(alpha = 0.12, eta0 = 0.04, learning_rate = "adaptive", tol = 0.001,  max_iter=1000)
 
 else:
     print("Error: Input one of the following models listed in help")
     quit()
-
 
 classifier.fit(trainX, trainY)
 preds = classifier.predict(testX)
@@ -65,6 +75,7 @@ cv2.waitKey(0)
 
 face = np.ndarray.flatten(face)
 face = np.expand_dims(face, 0)
+print(face.shape)
 face_pred = classifier.predict(face)
 print(face_data.target_names[face_pred])
 
