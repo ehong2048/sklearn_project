466a3391f90c78bc026de679bbf0c551d4fc6c68
ehong2048 <ehong2048@gmail.com>
2021-11-13 21:19:14 -0800

    Loaded lfw datset

--- face_classification.py
+++ face_classification.py
@@ -5,3 +5,8 @@ from sklearn.metrics import plot_confusion_matrix, mean_squared_error, r2_score
 from matplotlib import pyplot as plt
 import numpy as np
 
+print("Celebrity Facial Classification")
+lfw_people = fetch_lfw_people(min_faces_per_person=60) #fetches face data, only keeps people with at least 70 pictures
+num_images = {lfw_people.images.shape[0]}
+print(f'Num Images: {num_images}\nImage Size: {lfw_people.images.shape[1]} x {lfw_people.images.shape[2]}')
+
