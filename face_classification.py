6375c61ef4d9d23a5f07fff6bc35bbff2c042340
ehong2048 <ehong2048@gmail.com>
2021-11-13 21:41:35 -0800

    Divided into training and test set, classified with Logistic Regression

--- face_classification.py
+++ face_classification.py
@@ -1,12 +1,21 @@
-from sklearn.preprocessing import StandardScaler
+#from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import fetch_lfw_people
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import plot_confusion_matrix, mean_squared_error, r2_score
 from matplotlib import pyplot as plt
 import numpy as np
 
 print("Celebrity Facial Classification")
-lfw_people = fetch_lfw_people(min_faces_per_person=60) #fetches face data, only keeps people with at least 70 pictures
-num_images = {lfw_people.images.shape[0]}
-print(f'Num Images: {num_images}\nImage Size: {lfw_people.images.shape[1]} x {lfw_people.images.shape[2]}')
+face_data = fetch_lfw_people(min_faces_per_person=100, resize = 0.4) #fetches face data, only keeps people with at least 70 pictures
+num_images = face_data.images.shape[0]
+print(f'Num Images: {num_images}\nImage Shape: {face_data.images.shape[1]} by {face_data.images.shape[2]}')
 
+trainX, testX, trainY, testY = train_test_split(
+    face_data.data, face_data.target, test_size = 0.3, shuffle = True)
+
+classifier = LogisticRegression('l2', max_iter=2000) #later fine-tune for better results
+classifier.fit(trainX, trainY)
+preds = classifier.predict(testX)
+
+print(preds.shape)
