a7dd2439d1d15dae3850570be374a0f5619cac2c
ehong2048 <ehong2048@gmail.com>
2021-11-15 08:52:15 -0800

    Added color as dimension and debugged errors

--- face_classification.py
+++ face_classification.py
@@ -16,7 +16,8 @@ parser.add_argument('-i', "--image", help = "str path to image of Powell, Rumsfe
 args = vars(parser.parse_args())
 model = args["model"]
 
-face_data = fetch_lfw_people(min_faces_per_person=100, resize = 0.4) #fetches face data, only keeps people with at least 70 pictures
+face_data = fetch_lfw_people(min_faces_per_person=140, resize = 0.4, color = True) #fetches face data, only keeps people with at least 70 pictures
+print(face_data.data.shape)
 num_images = face_data.images.shape[0]
 h = face_data.images.shape[1]
 w = face_data.images.shape[2]
@@ -42,13 +43,13 @@ elif model == "LogisticRegressionCV":
     classifier = LogisticRegressionCV(penalty = 'l2', max_iter=100, solver = 'liblinear')
 
 elif model == "Perceptron":
-    classifier = Perceptron(penalty = 'l1', alpha = 0.14, max_iter=100)
+    classifier = Perceptron(penalty = 'l1', alpha = 0.14, max_iter=100, shuffle = False)
 
 elif model == "PassiveAggressiveClassifier":
-    classifier = PassiveAggressiveClassifier(max_iter=100)
+    classifier = PassiveAggressiveClassifier(max_iter=100, shuffle = False)
 
 elif model == "SGDClassifier":
-    classifier = SGDClassifier(alpha = 0.12, eta0 = 0.04, learning_rate = "adaptive", tol = 0.001,  max_iter=1000)
+    classifier = SGDClassifier(alpha = 0.12, eta0 = 0.04, learning_rate = "adaptive", tol = 0.001,  max_iter=1000, shuffle = False)
 
 else:
     print("Error: Input one of the following models listed in help")
@@ -67,8 +68,8 @@ plt.show()
 
 face_path = args["image"]
 face = cv2.imread(face_path)
-dim = (face_data.images.shape[1], face_data.images.shape[2])
-face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+dim = (face_data.images.shape[2], face_data.images.shape[1])
+# face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
 face = cv2.resize(face, dim, interpolation = cv2.INTER_AREA)
 cv2.imshow(f'Face to classify', face)
 cv2.waitKey(0)
@@ -77,5 +78,6 @@ face = np.ndarray.flatten(face)
 face = np.expand_dims(face, 0)
 print(face.shape)
 face_pred = classifier.predict(face)
+print(face_pred)
 print(face_data.target_names[face_pred])
 
