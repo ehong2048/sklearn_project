4507275b804eb80cfd5da53872db337862e90bb9
ehong2048 <ehong2048@gmail.com>
2021-11-30 01:40:15 -0800

    Added comments to face_classification.py

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Author: Emma Hong
 11/2021 Open Scikit Learn Project to demonstrate understanding
 
 ## Description
-I use skikit-learn's Labeled Faces in the Wild dataset to train a classification model for classifying images of Bush, Powell, and Blair, acheiving an accuracy of 90%. Then, I use it to classify downloaded images of the 3 people from Google images to see if it works.
+I use scikit-learn's Labeled Faces in the Wild dataset to train a classification model for classifying images of Bush, Powell, and Blair, acheiving an accuracy of 90%. Then, I use it to classify downloaded images of the 3 people from Google images to see if it works.
 
 ## Usage
     python face_classification.py --model $model_name$ --image $path_image_to_classify$
@@ -15,7 +15,8 @@ model_name are any of the following strings: LogisticRegression, LogisticRegress
 (Each model was fine-tuned to optimize the accuracy. I also made Shuffle = False so that the accuracy stayed the same for different trials while fine-tuning.)
 
 * Accuracy: fraction of correct predictions over number of samples
-Balanced_Accuracy: Macro average of recall (accounts for imbalanced datasets such as this one, where Bush images dominate)
+
+* Balanced_Accuracy: Macro average of recall (accounts for imbalanced datasets such as this one, where Bush images dominate)
 
 * Precision: ability of the classifier not to label as positive a sample that is negative (best is 1)
     * P = tp / (tp + fp) where tp is true positives and fp is false positives
@@ -42,20 +43,21 @@ LogisticRegressionCV:
 
 PassiveAggressiveClassifier:
 * Accuracy = 88.3%, Balanced_Accuracy = 81.7%
-* (0.97 Bush Recall but 0.86 precision due to lots of false positives for Bush, i.e. predicted Bush a lot. Also caused low recall for Powell and Blair as a result of falsely labelling them as Bush.)
+* (0.97 Bush recall value but 0.86 precision due to lots of false positives for Bush, i.e. predicted Bush a lot. Also caused low recall for Powell and Blair as a result of falsely labelling them as Bush.)
 
 Perceptron:
 * Accuracy = 81.7%, Balanced_Accuracy = 70.6%
-* (0.98 Bush Recall but 0.77 precision due to lots of false positives for Bush, i.e. predicted Bush a lot. Also caused low recall for Powell and Blair as a result of falsely labelling them as Bush.)
+* (0.98 Bush recall value but 0.77 precision due to lots of false positives for Bush, i.e. predicted Bush a lot. Also caused low recall for Powell and Blair as a result of falsely labelling them as Bush.)
 
 ***SGDClassifier:***
 * ***Accuracy = 90.1%, Balanced_Accuracy = 86.7%***
 * (Slightly faster than LogisticRegression)
 
-***For all of the models, Bush had consistently higher metrics (precision, recall, f1-score) for the most part, which makes sense considering that he had 530 total images in the dataset compared to 236 for Powell and 144 for Blair. This reflects the biases inherent in data. This translates into the real world negatively by ingraining prejudices into the system (ex. IBM Facial Recognition failing to identify darker-skinned women).***
+***For all of the models, Bush had consistently higher metrics (precision, recall, f1-score) for the most part, which makes sense considering that he had 530 total images in the dataset compared to 236 for Powell and 144 for Blair. This reflects the biases inherent in this data, due to the fact that these people appeared in the news the most often during 2007. This translates into the real world negatively by ingraining prejudices into the system (ex. IBM Facial Recognition failing to identify darker-skinned women).***
 
-## SGD Classifier Detailed Metrics
+# SGD Classifier Detailed Metrics
 Accuracy = 90.1%, Balanced_Accuracy = 86.7%
+
 Classification Report:
 
 |    Target    |  Prec. | Recall |   F1   |  Supp. |
--- face_classification.py
+++ face_classification.py
@@ -2,41 +2,35 @@ from sklearn.datasets import fetch_lfw_people
 from sklearn.externals._pilutil import imread
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV, Perceptron, PassiveAggressiveClassifier, SGDClassifier
-from sklearn.metrics import accuracy_score, plot_confusion_matrix
+from sklearn.metrics import accuracy_score, plot_confusion_matrix, classification_report, balanced_accuracy_score
 from matplotlib import pyplot as plt
 import argparse
 import numpy as np
 import cv2
 
-print("---Celebrity Facial Classification---")
+print("\n---Politician Facial Classification---")
 
-# parse arguments for which classification model and string path to face image to classify
+# parse arguments for which classification model and for string path to what face image to classify
 parser = argparse.ArgumentParser()
 parser.add_argument('-m', "--model", required = True, help = "string with which classification model to use: LogisticRegression, LogisticRegressionCV, PassiveAggressiveClassifier, Perceptron, or SGDClassifier")
-parser.add_argument('-i', "--image", help = "str path to image of Powell, Rumsfeld, Bush, or Schroeder to classify")
+parser.add_argument('-i', "--image", help = "str path to image of Powell, Bush, or Blair to classify")
 args = vars(parser.parse_args())
 model = args["model"]
 
 # loads face data and prints information
-# face_data = fetch_lfw_people(data_home="./test-dl", min_faces_per_person=140, resize = 0.4, color = False) #fetches face data, only keeps people with at least 70 pictures
-face_data = fetch_lfw_people(data_home="./test-dl", min_faces_per_person=140, color = True) #fetches face data, only keeps people with at least 70 pictures
-print(face_data.data.shape)
-print(face_data.data[0].shape)
-print(face_data.data[0])
-#cv2.imshow(f'Test face', face_data.images[0])
+face_data = fetch_lfw_people(data_home="./test-dl", min_faces_per_person=140, color = True) # fetches face data, only keeps people with at least 140 pictures (Bush, Blair, Powell)
 num_images = face_data.images.shape[0]
 h = face_data.images.shape[1]
 w = face_data.images.shape[2]
 print(f'Num Images: {num_images}\nImage Shape: {h} by {w}')
-print(f'Featured Celebrities: {face_data.target_names}')
+labels = face_data.target_names # gets the labels of the data
+print(f'Featured Politicians: {labels}')
 
+# splits data into test set and training set
 trainX, testX, trainY, testY = train_test_split(
     face_data.data, face_data.target, test_size = 0.3, shuffle = False)
-print("Actual identities of test set")
-print(testY)
 
-
-# creates classification model 
+# creates classification model (each one is fine-tuned)
 if model == "LogisticRegression":
     classifier = LogisticRegression('l2', max_iter=100, solver = 'liblinear')
 
@@ -56,55 +50,49 @@ else:
     print("Error: Input one of the following models listed in help")
     quit()
 
-# fits and trains model, then gets predication for test set
+# fits and trains model, then gets prediction for test set
 classifier.fit(trainX, trainY)
 preds = classifier.predict(testX)
-print("Predicted identities of test set")
-print(preds)
-print(preds.shape)
 
-# prints accuracy for test set and displays confusion matrix
+# prints metrics for test set and displays confusion matrix
+print("\n---Classification Model Metrics---")
 accuracy = accuracy_score(testY, preds)
 print(f'Accuracy: {accuracy}')
+balanced_accuracy = balanced_accuracy_score(testY, preds)
+print(f'Balanced Accuracy (Average Recall): {balanced_accuracy}')
+report = classification_report(testY, preds, target_names=labels)
+print(f'\nClassification Report:\n{report}')
 plot_confusion_matrix(classifier, testX, testY)
 plt.show()
 
 
-# loads face image based on passed in str path
-face_path = args["image"]
-img = imread(face_path)
-cv2.imshow(f'Face to classify', (img))
-cv2.waitKey()
-#slice_ = (slice(70, 195), slice(78, 172))
-#face = np.asarray(img[slice_], dtype = np.float32)
-face = np.asarray(img, dtype = np.float32)
-print(f'face shape: {face.shape}')
-face /= 255.0
-print(face.tolist()[0:1])
-dim = (face_data.images.shape[2], face_data.images.shape[1])
-face = cv2.resize(face, dim, interpolation = cv2.INTER_AREA) # resizes to same size as face data
-
-# flattens image to be same as same shape as face data
-print(face.shape)
-#face = face.reshape(1, -1)
-face = np.array(face).flatten().reshape(1, -1)
-print(face.shape)
-print(face)
-face_pred = classifier.predict(face)
-# face_pred = classifier.predict(testX[7].reshape(1, -1)) # predicts whose face it is
-print(face_pred)
-print(face_data.target_names[face_pred])
-
-print("====")
-#test = testX[4].reshape(1, -1)
+print("\n---Sanity Check: Testing Classifier on Image from actual Kaggle Dataset---")
 test = face_data.data[10].reshape(1, -1)
-print(test.shape)
-print(test)
-face_pred = classifier.predict(test) # predicts whose face it is
-print(face_pred)
-print(face_data.target_names[face_pred])
-cv2.imshow("test", (test/255.0).reshape(62, 47, 3))
+face_pred = classifier.predict(test)
+print(f'Target: {face_data.target_names[face_data.target[10]]}')
+print(f'Predicted: {face_data.target_names[face_pred]}')
+cv2.imshow("test", (test/255.0).reshape(62, 47, 3)) # have to divide by 255 so that we can see image (caused by how data is processed in fetch function)
 cv2.waitKey(0)
 
 
-print(94-1+2)
+
+print("\n---Sanity Check 2: Testing Classifier on Image from Google---")
+# loads face image based on passed in str path
+face_path = args["image"]
+img = imread(face_path) # uses sklearn's imread function (to be consistent with sklearn's _lfw_people function), which processes it differently 
+cv2.imshow(f'Face to classify', img)
+cv2.waitKey()
+
+# transforms image to be consistent with Kaggle dataset
+face = np.asarray(img, dtype = np.float32)
+print(f'Face shape: {face.shape}')
+face /= 255.0 # dividies by 255 (which is how it's processed in sklearn's fetch_lfw_people function)
+dim = (face_data.images.shape[2], face_data.images.shape[1])
+face = cv2.resize(face, dim, interpolation = cv2.INTER_AREA) # resizes to same size as Kaggle's face data
+face = np.array(face).flatten().reshape(1, -1) # flattens to be 1-D like Kaggle data
+print(f'New face shape: {face.shape}')
+
+# uses transformed image data to predict its politician identity
+face_pred = classifier.predict(face)
+print(f'Predicted: {face_data.target_names[face_pred]}')
+
